451b323d9ff11ea88ec19df2af0a2a772a18d6bf
Mohamed Guerroui <guerroui.m@gmail.com>
2020-11-08 14:02:32 +0100

    ajout des users

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,23 @@
 <template>
   <div id="app">
-    {{ users }}
+    <div class="ui four column grid">
+      <div class="column" v-for="user in users" :key="user.id">
+        <div class="ui card">
+          <div class="content">
+            <div class="header">
+              <input type="text" v-model="user.name" style="width:100%">
+            </div>
+            <div class="meta">
+              <a>Friends</a>
+            </div>
+            <div class="description">
+              {{ user.address.street }}<br>
+              {{ user.address.zipcode }} {{ user.address.city }}<br>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,6 +27,13 @@ export default {
     return {
       users: []
     }
+  },
+  mounted () {
+    this.$http.get('https://jsonplaceholder.typicode.com/users').then((response) => {
+      this.users = response.data
+    }, (response) => {
+      console.log('erreur', response)
+    })
   }
 }
 </script>
