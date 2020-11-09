9c92e3daef0a443d05030560ded7001b06800b5c
Mohamed Guerroui <guerroui.m@gmail.com>
2020-11-09 12:05:42 +0100

    ajout du loading

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="app">
+    <div class="ui active dimmer" v-if="loading">
+      <div class="ui loader"></div>
+    </div>
     <div class="ui four column grid">
       <div class="column" v-for="user in users" :key="user.id">
         <div class="ui card">
@@ -15,6 +18,12 @@
               {{ user.address.zipcode }} {{ user.address.city }}<br>
             </div>
           </div>
+          <div class="extra content">
+            <div class="ui two button">
+              <div class="ui basic green button" @click="save(user)">Modifier</div>
+              <div class="ui basic red button" @click="destroy(user)">Supprimer</div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -25,15 +34,31 @@
 export default {
   data () {
     return {
-      users: []
+      users: [],
+      loading: false
     }
   },
   mounted () {
-    this.$http.get('https://jsonplaceholder.typicode.com/users').then((response) => {
+    this.$http.get('https://jsonplaceholder.typicode.com/users/').then((response) => {
       this.users = response.data
     }, (response) => {
       console.log('erreur', response)
     })
+  },
+  methods: {
+    save (user) {
+      this.loading = true
+      this.$http.put('https://jsonplaceholder.typicode.com/users/' + user.id, {
+        name: user.name
+      }).then((response) => {
+      }, (response) => {
+        console.log('erreur', response)
+      }).then(_ => {
+        this.loading = false
+      })
+    },
+    destroy (user) {
+    }
   }
 }
 </script>
